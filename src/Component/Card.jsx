f8b86454a596fe26d6bbb570a2ab7531a19ad1f6
adarsh singh <adarshmaha987@gmail.com>
2024-01-13 12:56:59 +0530

    added search

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import "../App.css"
+import React from "react";
+import "../App.css";
+import { FaLocationDot } from "react-icons/fa6";
 
-const Card = () => {
+import { BsCake2 } from "react-icons/bs";
+import { IoIosCall } from "react-icons/io";
+import { MdOutlineMailOutline } from "react-icons/md";
+
+const Card = ({ data, searchUser }) => {
+  console.log("child",data);
+
+  const filteredData = data.filter((userData) => {
+    const fullName = `${userData.firstName} ${userData.lastName}`;
+    return fullName.toLowerCase().includes(searchUser.toLowerCase());
+  });
+
+
   return (
-    <div className='Container'>Card</div>
-  )
-}
+    <div className="cardContainer">
+      {filteredData.map((userData, id) => (
+        <div key={id} className="cardBox">
+          
+          {console.log("ppp", userData)}
+          <div className={`nameContainer ${userData.gender === "male" ?  "mbg" : "fbg"}`}>
+            <div className="contentBox">
+              <div className="nameheading">
+                {userData.firstName} {userData.lastName}
+              </div>
 
-export default Card
+              <div className="iconBox">
+                <FaLocationDot size="0.8vw" style={{ color: "white" }} />
+
+                <span>{userData.address.city}</span>
+              </div>
+
+              <div className="iconBox2">
+                <BsCake2 size="0.8vw" style={{ color: "white" }} />
+                <span>{userData.birthDate}</span>
+              </div>
+            </div>
+          </div>
+
+          <div className="detailContainer">
+            <div className="detales">
+              Choose the approach that fits your needs and coding style.
+            </div>
+          </div>
+          <div className="contactContainer">
+            <div className="iconBox3">
+              <IoIosCall size="0.8vw" style={{ color: "white" }} />
+
+              <span>{userData.phone}</span>
+            </div>
+            <div className="iconBox3">
+              <MdOutlineMailOutline size="0.8vw" style={{ color: "white" }} />
+
+              <span>{userData.email}</span>
+            </div>
+          </div>
+          <div className="avtarBox">
+            <img src={userData.image} alt="avtar" />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Card;
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaLocationDot } from "react-icons/fa6";
+import axios from "axios";
 
 import { BsCake2 } from "react-icons/bs";
 import { IoIosCall } from "react-icons/io";
 import { MdOutlineMailOutline } from "react-icons/md";
+import Card from "./Component/Card";
 
 const HomePage = () => {
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [searchUser,setSearchUser] = useState("")
+
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/users")
+      .then((response) => {
+        setUserData(response);
+      })
+      .catch((error) => {
+        console.log("Error while fetching data ", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+  console.log("hshds",userData.data && userData.data.users)
   return (
     <div className="mainContainer">
       <div className="contentContainer">
@@ -14,50 +34,17 @@ const HomePage = () => {
         </div>
         <div className="searchBox">
           <div className="inputBox">
-            <input type="text" placeholder="search people.." />
+            <input 
+            type="text" 
+            placeholder="search people.." 
+            value={searchUser}
+            onChange={(e)=>setSearchUser(e.target.value)}
+            />
+            {console.log("mjxjx",searchUser)}
           </div>
         </div>
-        <div className="cardContainer">
-          <div className="cardBox">
-            <div className="nameContainer">
-              <div className="contentBox">
-                <div className="heading">Alfred Nobel</div>
-
-                <div className="iconBox">
-                  <FaLocationDot size="0.8vw" style={{ color: "white" }} />
-
-                  <span>Greece</span>
-                </div>
-
-                <div className="iconBox2">
-                  <BsCake2 size="0.8vw" style={{ color: "white" }} />
-                  <span>6 Feb 1945</span>
-                </div>
-              </div>
-            </div>
-
-            <div className="detailContainer">
-              <div className="detales">
-                Choose the approach that fits your needs and coding style.
-              </div>
-            </div>
-            <div className="contactContainer">
-            <div className="iconBox3">
-                  <IoIosCall  size="0.8vw" style={{ color: "white" }} />
-
-                  <span>73865635</span>
-                </div>
-                <div className="iconBox3">
-                  <MdOutlineMailOutline size="0.8vw" style={{ color: "white" }} />
-
-                  <span>adarshmaha987@gmail.com</span>
-                </div>
-            </div>
-            <div className='avtarBox'>
-                        <img src="https://robohash.org/Sheldon.png?set=set4" alt="avtar" />
-                    </div>
-          </div>
-        </div>
+     
+        {loading ? <p>Loding</p> : <Card data={userData.data && userData.data.users} searchUser={searchUser}/>}
       </div>
     </div>
   );
